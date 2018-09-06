1b33bab1e521127c9101e47ff0e3624c1a102910
berrybretch <berrybretch@gmail.com>
2018-09-06 20:55:15 +0300

    Added endpoints for GET /orders and GET /orders/<specific_order> Added test for GET endpoints

---
+++ tests/get_endpoints_test.py
@@ -0,0 +1,25 @@
+import unittest
+from flask import Flask
+from get_endpoints import *
+from resources import resources
+'''
+Tests for Api endpoints
+'''
+
+
+class Test_Endpoints(unittest.TestCase):
+
+    def test_client_get(self):
+        '''
+                Testing if the GET  requests return the proper status codes
+        '''
+        dummy = APP.test_client(self)
+        self.assertEqual(dummy.get('/orders').status_code, 200)  # OK
+        self.assertEqual(dummy.get('/orders/12').status_code, 200)
+        self.assertEqual(dummy.get('/orders/204863').status_code, 200)
+        self.assertEqual(
+            dummy.get('/orders/23516648').status_code, 404)  # Not Found
+        self.assertEqual(
+            dummy.get('/orders/ericmwachala').status_code, 404)
+        self.assertEqual(dummy.get('/order/204863').status_code, 404)
+        self.assertEqual(dummy.get('/orders/12').status_code, 200)
