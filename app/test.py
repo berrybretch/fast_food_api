5bc18f9bd07eb6723d3d07a47cebd7a796672417
mwachalaEric <32340080+berrybretch@users.noreply.github.com>
2018-09-07 23:47:02 +0300

    Update test.py

--- app/test.py
+++ app/test.py
@@ -1,20 +1,22 @@
 import unittest
-from flask import Flask  
+from flask import Flask
 import json
 from app import APP
-class TestPost(unittest.TestCase):
 
+
+class TestPost(unittest.TestCase):
 
     def test_client_post(self):
         '''
                 Testing if the POST requests return the proper status codes
         '''
         dummy = APP.test_client(self)
-        payload = {"order_id": 19,
-                   "order_content": "Little Chicken",
-                   "user": "Eric",
-                   "order_status": "Denied",
+        payload = {"order_id": 000000000,
+                   "order_content": "TestChicken",
+                   "user": "Test",
+                   "order_status": "Tests"
                    }
-        response = dummy.post(json.dumps(payload), headers={
-                              'content_type': 'application/json'})
+        response = dummy.post('/orders',  data=json.dumps(payload),
+                              content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        
