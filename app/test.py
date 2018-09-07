c006c16fe0915ff4b711428b8a9c9dd106bc6e49
berrybretch <berrybretch@gmail.com>
2018-09-07 14:14:54 +0300

    POST tests

--- app/test.py
+++ app/test.py
@@ -11,10 +11,28 @@ class TestEndpoints(unittest.TestCase):
 
     def test_client_post(self):
         '''
-                Testing if the GET  requests return the proper status codes
+                Testing if the POST  requests return the proper status codes
         '''
         payload = {"order_id": 19,
-                   "order_content": "Little Chicken",
-                   "user": "Eric",
-                   "order_status": "Denied",
+                   "order_content": "TestPhrase",
+                   "user": "testphrase",
+                   "order_status": "testphrase",
                    }
+        url = 'http://127.0.0.1:5000/orders'
+        dummy = requests.post(url, json=payload)
+        self.assertEqual(dummy.status_code, 200)
+
+    def test_post_content(self):
+        '''
+        Test the content of the POST request
+        '''
+        payload = {"order_id": 900,
+                   "order_content": "TestPhraseOne",
+                   "user": "testphraseTwo",
+                   "order_status": "testphraseThree",
+                   }
+        url = 'http://127.0.0.1:5000/orders'
+        dummy = requests.post(url, json=payload)
+        self.assertIn('TestPhraseOne', dummy.text)
+        self.assertIn('testphraseTwo', dummy.text)
+        self.assertIn('testphraseThree', dummy.text)
